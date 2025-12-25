61514d7db8a697cd282d2c4e12f99a8babf28ec0
MariamKochadze <kochadzemariam80@gmail.com>
2025-12-25 19:06:52 +0400

    fix: add users

--- app/lib/placeholder-data.ts
+++ app/lib/placeholder-data.ts
@@ -1,4 +1,32 @@
-export const users = [];
-export const customers = [];
-export const invoices = [];
-export const revenue = [];
+
+export const users = [
+  {
+    id: '410544b2-4001-4271-9855-fec4b6a6442a',
+    name: 'User One',
+    email: 'user@example.com',
+    password: '123456',
+  },
+];
+
+export const customers = [
+  {
+    id: 'd6e15727-9fe1-4961-8c5b-ea44a9bd81aa',
+    name: 'Acme Corp',
+    email: 'info@acme.com',
+    image_url: '/customers/acme.png',
+  },
+];
+
+export const invoices = [
+  {
+    customer_id: 'd6e15727-9fe1-4961-8c5b-ea44a9bd81aa',
+    amount: 15795,
+    status: 'paid',
+    date: '2024-01-10',
+  },
+];
+
+export const revenue = [
+  { month: 'Jan', revenue: 2000 },
+  { month: 'Feb', revenue: 1800 },
+];
